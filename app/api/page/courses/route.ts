ce98f94565b9ecbb0aacb08383d2f8cc46f1f797
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-02-28 15:50:01 +0530

    fetch all courses in home screen

---
+++ app/api/page/courses/route.ts
@@ -0,0 +1,22 @@
+import { db } from "@/lib/db/db";
+import { Course } from "@/lib/schema/schema";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const getAllCourses = await db
+      .select({
+        title: Course.courseName,
+        description: Course.courseShortDescription,
+        instructor: Course.courseInstrutor,
+        seats: Course.studentCapacity,
+        image: Course.courseImageURL,
+        id: Course.id,
+      })
+      .from(Course);
+    return NextResponse.json({success: true, message: "Fetched", courses: getAllCourses})
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({success: false, message: "Something went wrong"})
+  }
+}
