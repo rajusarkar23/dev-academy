a58605c54c4dd9eeb9c466bd9f7a09b292afdf76
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-02-28 16:41:17 +0530

    testimonial comp: improve color

--- components/page/Testimonials.tsx
+++ components/page/Testimonials.tsx
@@ -1,20 +1,16 @@
 export default function Testimonials() {
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section className="bg-gradient-to-br from-black to-violet-900/80 mt-8">
       <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6" />
       <div className="mx-auto max-w-screen-sm">
-        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+        <h2 className="mb-4 text-6xl tracking-tight font-extrabold underline underline-offset-3 text-center">
           Testimonials
         </h2>
-        <p className="mb-8 font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">
-          Explore the whole collection of open-source web components and
-          elements built with the utility classes from Tailwind
-        </p>
       </div>
       <div className="grid mb-8 lg:mb-12 lg:grid-cols-2">
-        <figure className="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 md:p-12 lg:border-r dark:bg-gray-800 dark:border-gray-700">
-          <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500 dark:text-gray-400">
-            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+        <figure className="flex flex-col justify-center items-center p-8 text-center border-b border-gray-200 md:p-12 lg:border-r">
+          <blockquote className="mx-auto mb-8 max-w-2xl text-gray-100">
+            <h3 className="text-lg font-semibold text-gray-900">
               Speechless with how easy this was to integrate
             </h3>
             <p className="my-4">
@@ -38,17 +34,17 @@ export default function Testimonials() {
               src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png"
               alt="profile picture"
             />
-            <div className="space-y-0.5 font-medium dark:text-white text-left">
+            <div className="space-y-0.5 font-medium text-left">
               <div>Bonnie Green</div>
-              <div className="text-sm font-light text-gray-500 dark:text-gray-400">
+              <div className="text-sm font-light text-gray-100">
                 Developer at Open AI
               </div>
             </div>
           </figcaption>
         </figure>
-        <figure className="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 md:p-12 dark:bg-gray-800 dark:border-gray-700">
-          <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500 dark:text-gray-400">
-            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+        <figure className="flex flex-col justify-center items-center p-8 text-center border-b border-gray-200 md:p-12  ">
+          <blockquote className="mx-auto mb-8 max-w-2xl text-gray-100">
+            <h3 className="text-lg font-semibold text-gray-100">
               Solid foundation for any project
             </h3>
             <p className="my-4">
@@ -69,17 +65,17 @@ export default function Testimonials() {
               src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/roberta-casas.png"
               alt="profile picture"
             />
-            <div className="space-y-0.5 font-medium dark:text-white text-left">
+            <div className="space-y-0.5 font-medium text-left">
               <div>Roberta Casas</div>
-              <div className="text-sm font-light text-gray-500 dark:text-gray-400">
+              <div className="text-sm font-light text-gray-100">
                 Lead designer at Dropbox
               </div>
             </div>
           </figcaption>
         </figure>
-        <figure className="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 lg:border-b-0 md:p-12 lg:border-r dark:bg-gray-800 dark:border-gray-700">
-          <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500 dark:text-gray-400">
-            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+        <figure className="flex flex-col justify-center items-center p-8 text-center border-b border-gray-200 lg:border-b-0 md:p-12 lg:border-r">
+          <blockquote className="mx-auto mb-8 max-w-2xl text-gray-100 ">
+            <h3 className="text-lg font-semibold text-gray-900 ">
               Mindblowing workflow and variants
             </h3>
             <p className="my-4">
@@ -102,17 +98,17 @@ export default function Testimonials() {
               src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
               alt="profile picture"
             />
-            <div className="space-y-0.5 font-medium dark:text-white text-left">
+            <div className="space-y-0.5 font-medium text-left">
               <div>Jese Leos</div>
-              <div className="text-sm font-light text-gray-500 dark:text-gray-400">
+              <div className="text-sm font-light text-gray-500">
                 Software Engineer at Facebook
               </div>
             </div>
           </figcaption>
         </figure>
-        <figure className="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-gray-200 md:p-12 dark:bg-gray-800 dark:border-gray-700">
-          <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500 dark:text-gray-400">
-            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+        <figure className="flex flex-col justify-center items-center p-8 text-center border-gray-200 md:p-12">
+          <blockquote className="mx-auto mb-8 max-w-2xl text-gray-100">
+            <h3 className="text-lg font-semibold text-gray-900">
               Efficient Collaborating
             </h3>
             <p className="my-4">
@@ -131,22 +127,14 @@ export default function Testimonials() {
               src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/joseph-mcfall.png"
               alt="profile picture"
             />
-            <div className="space-y-0.5 font-medium dark:text-white text-left">
+            <div className="space-y-0.5 font-medium text-left">
               <div>Joseph McFall</div>
-              <div className="text-sm font-light text-gray-500 dark:text-gray-400">
+              <div className="text-sm font-light text-gray-100">
                 CTO at Google
               </div>
             </div>
           </figcaption>
         </figure>
-      </div>
-      <div className="text-center">
-        <a
-          href="#"
-          className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-        >
-          Show more...
-        </a>
       </div>
     </section>
   );
