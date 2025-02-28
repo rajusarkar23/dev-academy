a721510f320f4b529cd091087a11a4eb633fac46
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-02-28 14:32:26 +0530

    fix ui for course slider and place links

--- components/page/Courses.tsx
+++ components/page/Courses.tsx
@@ -1,74 +1,89 @@
-"use client"
-import Image from "next/legacy/image"
-import { Autoplay } from "swiper/modules"
-import { Swiper, SwiperSlide } from "swiper/react"
+"use client";
+import Image from "next/legacy/image";
+import { Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/effect-fade';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
 import Link from "next/link";
 
 export default function Courses() {
+  // fetch courses by slug
 
-    const courses = [
-        {
-            title: "Complete web development course",
-            description: "Only web development course that you will need. Covers HTML, CSS, Tailwind, Node, React, MongoDB, Prisma, Deployment etc",
-            thumbnaileImageUrl: "https://pub-367a5b1b28f9415dae5b51f69d042dff.r2.dev/thumbImageUrl.jpg",
-            category: "Full-Stack Web Development"
-        },
-        {
-            title: "Complete web development course 2",
-            description: "Only web development course that you will need. Covers HTML, CSS, Tailwind, Node, React, MongoDB, Prisma, Deployment etc",
-            thumbnaileImageUrl: "https://pub-367a5b1b28f9415dae5b51f69d042dff.r2.dev/thumbImageUrl.jpg",
-            category: "Backend-End Web Development"
-        },
-    ]
+  const courses = [
+    {
+      title: "Unlock opportunities with this Master Docker course.",
+      description:
+        "Containers have revolutionized how modern applications are developed, deployed, and managed.",
+      thumbnaileImageUrl:
+        "https://pub-367a5b1b28f9415dae5b51f69d042dff.r2.dev/1_mktjwp",
+      category: "Docker mastery",
+      link: "/course/master-docker"
+    },
+    {
+      title: "Master React.js - Build Modern Web Applications",
+      description:
+        "Unlock the power of React.js, the most popular front-end library, and become a skilled web developer.",
+      thumbnaileImageUrl:
+        "https://pub-367a5b1b28f9415dae5b51f69d042dff.r2.dev/1_d61p4i",
+      category: "Master React.js",
+      link: "/course/master-reactjs"
+    },
+  ];
 
-    return (
-        <div className="mx-auto max-w-7xl p-4 xl:p-1">
-            <Swiper
-                spaceBetween={0}
-                slidesPerView={1}
-                centeredSlides={true}
-                autoplay={{
-                    delay: 3000,
-                    disableOnInteraction: false,
-                }}
-                modules={[Autoplay]}
-                className="h-[500px] w-full"
-
-            >
-                {
-                    courses.map((items, index) => (
-                        <SwiperSlide key={index}>
-                            <div className="h-full w-full rounded-lg bg-gradient-to-r from-black to-blue-900/50">
-                                <div className="flex items-center justify-center h-full text-white px-4 ">
-                                    <div className="space-y-3">
-                                        <p className="inline-flex items-center rounded-full bg-blue-500/40 px-4 py-1 text-sm text-white/80 font-semibold border border-green-300">{items.category}</p>
-                                        <p className="text-4xl">{items.title}</p>
-                                        <p className="text-sm w-72">{items.description}</p>
-                                        <div className="space-x-3">
-                                            <Link href={"/"} className="bg-blue-500 px-4 py-2 rounded-lg shadow-lg hover:scale-105 transition-all">Join Now </Link>
-                                            <Link href={"/"} className="bg-gray-600 px-4 py-2 rounded-lg shadow-lg hover:scale-105 transition-all">Learn</Link>
-                                        </div>
-                                    </div>
-                                    <div className="relative w-full h-40">
-                                        <Image
-                                            src={items.thumbnaileImageUrl}
-                                            alt={items.title}
-                                            layout="fill"
-                                            objectFit="cover"
-                                            className="rounded-lg"
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        </SwiperSlide>
-                    ))
-
-                }
-            </Swiper>
-        </div>
-    )
+  return (
+    <div className="mx-auto max-w-7xl p-4 xl:p-1">
+      <Swiper
+        spaceBetween={0}
+        slidesPerView={1}
+        centeredSlides={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay]}
+        className="h-[500px] w-full"
+      >
+        {courses.map((items, index) => (
+          <SwiperSlide key={index}>
+            <div className="h-full rounded-lg bg-gradient-to-r from-black to-blue-900/50 flex justify-center items-center px-4">
+              <div className="sm:flex text-white gap-4 space-y-3">
+                <div className="flex justify-center pt-10 px-1 sm:mb-8">
+                  <Image
+                    src={items.thumbnaileImageUrl}
+                    alt={items.title}
+                    className="rounded-lg"
+                    height={800}
+                    width={1600}
+                  />
+                </div>
+                <div className="space-y-3 flex flex-col justify-center">
+                  <p className="inline-flex items-center rounded-full bg-blue-500/40 px-4 py-1 text-sm text-white/80 font-semibold border border-green-300 w-64 text-center">
+                    {items.category}
+                  </p>
+                  <p className="text-4xl">{items.title}</p>
+                  <p className="text-sm w-72">{items.description}</p>
+                  <div className="space-x-3">
+                    <Link
+                      href={items.link}
+                      className="bg-blue-500 px-4 py-2 rounded-lg shadow-lg hover:scale-105 transition-all"
+                    >
+                      Join Now
+                    </Link>
+                    <Link
+                      href={items.link}
+                      className="bg-gray-600 px-4 py-2 rounded-lg shadow-lg hover:scale-105 transition-all"
+                    >
+                      Learn
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 }
--- components/page/Hero.tsx
+++ components/page/Hero.tsx
@@ -6,13 +6,13 @@ export default function Hero() {
         <div className="background-hero">
             <div className="max-w-6xl">
                 <div className="flex justify-center">
-                    <Link href={"/"} className="flex items-center bg-blue-400/40 px-3 py-2 shadow-md mb-1 rounded-full font-bold text-sm hover:scale-105 transition-all border border-green-300/70">Join this bootcamp <CircleArrowRight size={20} className="ml-2" /></Link>
+                    <Link href={"/course/full-stack-web-development"} className="flex items-center bg-blue-400/40 px-3 py-2 shadow-md mb-1 rounded-full font-bold text-sm hover:scale-105 transition-all border border-green-300/70">Join this bootcamp <CircleArrowRight size={20} className="ml-2" /></Link>
                 </div>
                 <h1 className="text-6xl text-center ">Master your full-stack web development skills with this bootcamp.</h1>
                 <p className="text-center">We will teach here evrything, that make you a good developer. This bootcamp include Recat, Next.js, Node.js, Databases like MongoDB/PostgreSQL</p>
                 <div className="flex justify-center mt-2 space-x-4 items-center">
                     <Link href={`/course/full-stack-web-development`} className="px-5 py-3 bg-blue-600 shadow-lg rounded-lg font-bold text-white/80 hover:scale-105 transition-all">Join Now</Link>
-                    <Link href={"/dashboard/123"} className="px-5 py-3 bg-yellow-100/40 rounded-lg font-bold text-white/80 hover:scale-105 transition-all shadow-lg">Learn</Link>
+                    <Link href={"/course/full-stack-web-development"} className="px-5 py-3 bg-yellow-100/40 rounded-lg font-bold text-white/80 hover:scale-105 transition-all shadow-lg">Learn</Link>
                 </div>
             </div>
         </div>
