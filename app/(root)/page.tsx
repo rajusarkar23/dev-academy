cbcdd241f4d44293b274084655f732f7d5c492ec
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-02-28 16:14:44 +0530

    add testimonials

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import AllCourses from "@/components/page/AllCourses";
 import Courses from "@/components/page/Courses";
 import Hero from "@/components/page/Hero";
 import OurStats from "@/components/page/OurStats";
+import Testimonials from "@/components/page/Testimonials";
 
 export default function Home() {
   return (
@@ -17,6 +18,9 @@ export default function Home() {
       </section>
       <section>
         <AllCourses />
+      </section>
+      <section>
+        <Testimonials />
       </section>
     </main>
   );
